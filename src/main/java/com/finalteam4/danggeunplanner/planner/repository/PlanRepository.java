2c2d271a9cae0ded5121acc756e8fd98002c6c10
suhjaesuk <suhjaesuk@gmail.com>
2023-02-14 19:45:37 +0900

    feat : 플래너  가독성 좋게 변경

--- src/main/java/com/finalteam4/danggeunplanner/planner/repository/PlanRepository.java
+++ src/main/java/com/finalteam4/danggeunplanner/planner/repository/PlanRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PlanRepository extends JpaRepository<Plan,Long> {
-    List<Plan> findAllByMember(Member member);
 }
--- src/main/java/com/finalteam4/danggeunplanner/planner/service/PlannerService.java
+++ src/main/java/com/finalteam4/danggeunplanner/planner/service/PlannerService.java
@@ -25,56 +25,37 @@ public class PlannerService {
     private final PlannerRepository plannerRepository;
     private final MemberRepository memberRepository;
 
-    public PlannerResponse findAll(Member member, String username, String date) {
-        Member other = memberRepository.findByUsername(username).orElseThrow(
-                () -> new DanggeunPlannerException(NOT_FOUND_MEMBER)
-        );
-        Optional<Planner> planner = plannerRepository.findByMemberAndDate(other, date);
-
-        if (planner.isPresent()) {
-            PlannerResponse response = new PlannerResponse(planner.get(), member);
-            for (Plan plan : planner.get().getPlans()) {
-                response.addPlan(new PlanResponse(plan));
-            }
-            for (Timer timer : planner.get().getTimers()) {
-                response.addTimer(new TimerResponse(timer));
-            }
-            return response;
-        }
-        return new PlannerResponse(other,member);
+    public PlannerResponse getPlanner(Member member, String username, String date) {
+        return getPlannerResponse(member, username, date, true, true);
     }
 
-    public PlannerResponse findPlan(Member member, String username, String date) {
-        Member other = memberRepository.findByUsername(username).orElseThrow(
-                () -> new DanggeunPlannerException(NOT_FOUND_MEMBER)
-        );
-        Optional<Planner> planner = plannerRepository.findByMemberAndDate(other, date);
-
-        if (planner.isPresent()) {
-            PlannerResponse response = new PlannerResponse(planner.get(), member);
-            for (Plan plan : planner.get().getPlans()) {
-                response.addPlan(new PlanResponse(plan));
-            }
-            return response;
-        }
-        return new PlannerResponse(other,member);
+    public PlannerResponse getPlan(Member member, String username, String date) {
+        return getPlannerResponse(member, username, date, true, false);
     }
 
-    public PlannerResponse findTimer(Member member, String username, String date) {
-        Member other = memberRepository.findByUsername(username).orElseThrow(
-                () -> new DanggeunPlannerException(NOT_FOUND_MEMBER)
-        );
-        Optional<Planner> planner = plannerRepository.findByMemberAndDate(other, date);
-
-        if (planner.isPresent()) {
-            PlannerResponse response = new PlannerResponse(planner.get(), member);
-            for (Timer timer : planner.get().getTimers()) {
-                response.addTimer(new TimerResponse(timer));
-            }
-            return response;
-        }
-        return new PlannerResponse(other,member);
+    public PlannerResponse getTimer(Member member, String username, String date) {
+        return getPlannerResponse(member, username, date, false, true);
     }
 
+    private PlannerResponse getPlannerResponse(Member member, String username, String date, boolean includePlans, boolean includeTimers) {
+        Member findMember = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new DanggeunPlannerException(NOT_FOUND_MEMBER));
 
+        return plannerRepository.findByMemberAndDate(findMember, date)
+                .map(planner -> {
+                    PlannerResponse response = new PlannerResponse(planner, member);
+                    if (includePlans) {
+                        for (Plan plan : planner.getPlans()) {
+                            response.addPlan(new PlanResponse(plan));
+                        }
+                    }
+                    if (includeTimers) {
+                        for (Timer timer : planner.getTimers()) {
+                            response.addTimer(new TimerResponse(timer));
+                        }
+                    }
+                    return response;
+                })
+                .orElse(new PlannerResponse(findMember, member));
+    }
 }
