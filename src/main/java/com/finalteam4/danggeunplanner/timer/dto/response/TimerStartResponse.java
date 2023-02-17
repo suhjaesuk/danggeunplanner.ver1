0dc468b599a26f2ba82ea05235826978829911ba
suhjaesuk <suhjaesuk@gmail.com>
2023-02-17 13:21:32 +0900

    feat : TimerStartResponse에 startTIme 추가

--- src/main/java/com/finalteam4/danggeunplanner/timer/dto/response/TimerStartResponse.java
+++ src/main/java/com/finalteam4/danggeunplanner/timer/dto/response/TimerStartResponse.java
@@ -3,11 +3,16 @@ package com.finalteam4.danggeunplanner.timer.dto.response;
 import com.finalteam4.danggeunplanner.timer.entity.Timer;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class TimerStartResponse {
     private final Long timerId;
+    private final LocalDateTime startTime;
 
     public TimerStartResponse(Timer timer){
         this.timerId = timer.getId();
+        this.startTime = timer.getStartTime();
     }
+
 }
