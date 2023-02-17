de0650587dc9f0ed5da5599c5c659b78d3ea9d49
suhjaesuk <suhjaesuk@gmail.com>
2023-02-17 13:24:26 +0900

    feat : TimerServiceTest 작성 -> 타이머 시작 시 서비스 로직 검증

---
+++ src/test/java/com/finalteam4/danggeunplanner/timer/service/TimerServiceTest.java
@@ -0,0 +1,71 @@
+package com.finalteam4.danggeunplanner.timer.service;
+
+import com.finalteam4.danggeunplanner.calendar.repository.CalendarRepository;
+import com.finalteam4.danggeunplanner.member.entity.Member;
+import com.finalteam4.danggeunplanner.member.service.MemberValidator;
+import com.finalteam4.danggeunplanner.planner.repository.PlannerRepository;
+import com.finalteam4.danggeunplanner.timer.dto.request.TimerStartRequest;
+import com.finalteam4.danggeunplanner.timer.dto.response.TimerStartResponse;
+import com.finalteam4.danggeunplanner.timer.entity.Timer;
+import com.finalteam4.danggeunplanner.timer.repository.TimerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class TimerServiceTest {
+
+    @Mock
+    private TimerRepository timerRepository;
+    @Mock
+    private PlannerRepository plannerRepository;
+    @Mock
+    private CalendarRepository calendarRepository;
+    @Mock
+    private MemberValidator memberValidator;
+
+    @InjectMocks
+    private TimerService timerService;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void startTimerTest() {
+
+        Member member = Member.builder()
+                .email("test@gmail.com")
+                .password("test1234!")
+                .username("test")
+                .profileImage("test")
+                .isPlannerOpened(false)
+                .build();
+
+        TimerStartRequest request = new TimerStartRequest();
+        request.setStartTime(LocalDateTime.of(2023, 1, 1, 0, 0, 0));
+        Timer timer = request.toTimer(member);
+
+        when(timerRepository.save(any())).thenReturn(timer);
+
+        // when
+        TimerStartResponse response = timerService.startTimer(member, request);
+
+        // then
+        assertThat(response.getStartTime()).isEqualTo(LocalDateTime.of(2023, 1, 1, 0, 0, 0));
+
+    }
+}
